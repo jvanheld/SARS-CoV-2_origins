cde7996ef128213d428854b3e4e331b68aa6edea
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2020-05-01 05:06:11 +0200

    added redundancy filtering with cd-hit to generate non-redundant collections

--- scripts/makefiles/03_protein-alignments.mk
+++ scripts/makefiles/03_protein-alignments.mk
@@ -7,17 +7,30 @@ MAKE=make -f ${MAKEFILE}
 
 targets:
 	@echo "Targets:"
+	@echo
 	@echo "	uniprot_seq			automatically retrieve spike protein sequences from Uniprot"
-	@echo "	uniprot_coronaviridae		run uniprot_seq with coronaviridae"
-	@echo "	uniprot_betacoronaviridae	run uniprot_seq with betacoronaviridae"
-	@echo "	uniprot_sarbecovirus		run uniprot_seq with Sarbecovirus"
-	@echo "	uniprot_sars			run uniprot_seq with SARS"
+	@echo "	uniprot_seq_sars		get spike sequences from Uniprot for SARS"
+#	@echo "	uniprot_seq_sarbecovirus	get spike sequences from Uniprot for Sarbecovirus"
+	@echo "	uniprot_seq_betacoronaviridae	get spike sequences from Uniprot for Betacoronaviridae"
+	@echo "	uniprot_seq_coronaviridae	get spike sequences from Uniprot for Coronaviridae"
+	@echo
+	@echo "	nr				generate non-retundant sequences with cd-hit"
+	@echo "	nr_sars				generate non-redundant sequences from Uniprot for SARS"
+#	@echo "	nr_sarbecovirus			generate non-redundant sequences from Uniprot for Sarbecovirus"
+	@echo "	nr_betacoronaviridae		generate non-redundant sequences from Uniprot for Betacoronaviridae"
+	@echo "	nr_coronaviridae		generate non-redundant sequences from Uniprot for Coronaviridae"
+	@echo
 	@echo "	align_muscle			align spike protein sequences with muscle"
-	@echo "	align_uniprot_seq		align spike proteins from Uniprot"
-	@echo "	align_uniprot_seq		align 22 selected sequences"
+#	@echo "	align_uniprot		align spike proteins from Uniprot"
+	@echo "	align_selected		align selection of representative sequences"
+	@echo "	align_genbank		align sequences from Genbank"
+	@echo "	align_uniprot_sars		align spike sequences from Uniprot for SARS"
+#	@echo "	align_uniprot_sarbecovirus	align spike sequences from Uniprot for Sarbecovirus"
+	@echo "	align_uniprot_betacoronaviridae	align spike sequences from Uniprot for Betacoronaviridae"
+	@echo "	align_uniprot_coronaviridae	align spike sequences from Uniprot for Coronaviridae"
+	@echo
 	@echo "	identify_insertions		locate the insertions in a chosen sequence"
-	@echo "	align_selected_seq		align selection of representative sequences"
-	@echo "	align_genbank_seq		align sequences from Genbank"
+
 
 ################################################################
 ## Retrieve sequences from Uniprot
@@ -33,9 +46,12 @@ UNIPROT_QUERY_SEQ=taxonomy:${TAXID}+AND+name:spike+AND+fragment:no&format=fasta
 UNIPROT_URL_TAB=https://www.uniprot.org/uniprot/?query=${UNIPROT_QUERY_TAB}
 UNIPROT_URL_SEQ=https://www.uniprot.org/uniprot/?query=${UNIPROT_QUERY_SEQ}
 SPIKE_SEQ_DIR=data/spike_proteins
-UNIPROT_PREFIX=${SPIKE_SEQ_DIR}/${TAXNAME}_${TAXID}_spike-proteins_uniprot
+SPIKE_PREFIX=${TAXNAME}_${TAXID}_spike-proteins_uniprot
+SPIKE_SEQ=${DATA_DIR}/${SPIKE_PREFIX}
+UNIPROT_PREFIX=${SPIKE_SEQ_DIR}/${SPIKE_PREFIX}
 UNIPROT_TAB=${UNIPROT_PREFIX}.tsv
 UNIPROT_SEQ=${UNIPROT_PREFIX}.fasta
+UNIPROT_SEQ_NB=`grep '^>' ${UNIPROT_SEQ} | wc -l`
 uniprot_seq:
 	@echo "Retrieving spike protein sequences from Uniprot"
 	@echo "	TAXNAME	${TAXNAME}"
@@ -48,59 +64,109 @@ uniprot_seq:
 	@echo "	UNIPROT_URL_SEQ		${UNIPROT_URL_SEQ}"
 	@curl "${UNIPROT_URL_SEQ}" > ${UNIPROT_SEQ}
 	@echo "	UNIPROT_SEQ		${UNIPROT_SEQ}"
+	@echo "	UNIPROT_SEQ_NB		${UNIPROT_SEQ_NB}"
 
 
-uniprot_coronaviridae:
-	@${MAKE} uniprot_seq TAXID=11118 TAXNAME=coronaviridae
-
-uniprot_betacoronaviridae:
-	@${MAKE} uniprot_seq TAXID=694002 TAXNAME=betacoronavirus
-
-uniprot_sarbecovirus:
-	@${MAKE} uniprot_seq TAXID=2509511 TAXNAME=sarbecovirus
-
-uniprot_sars:
+uniprot_seq_sars:
 	@${MAKE} uniprot_seq TAXID=694009 TAXNAME=SARS
 
+# uniprot_seq_sarbecovirus:
+# 	@${MAKE} uniprot_seq TAXID=2509511 TAXNAME=sarbecovirus
+
+uniprot_seq_betacoronaviridae:
+	@${MAKE} uniprot_seq TAXID=694002 TAXNAME=betacoronavirus
+
+uniprot_seq_coronaviridae:
+	@${MAKE} uniprot_seq TAXID=11118 TAXNAME=coronaviridae
+
+
+################################################################
+## Generate non-redundant collections of protein sequences
+NR_IDENTITY=0.99
+NR_THREADS=4
+NR_MEM=8000
+NR_SUFFIX=NR_${NR_IDENTITY}
+SPIKE_PREFIX_NR=${SPIKE_PREFIX}_${NR_SUFFIX}
+SEQ_PREFIX=${UNIPROT_PREFIX}
+SEQ=${SEQ_PREFIX}.fasta
+NR_PREFIX=${SEQ_PREFIX}_${NR_SUFFIX}
+NR_SEQ=${NR_PREFIX}.fasta
+SEQ_NB=`grep '^>' ${SEQ} | wc -l`
+NR_SEQ_NB=`grep '>' ${NR_SEQ} | wc -l`
+nr:
+	@echo "Generating non-redundant sequences"
+	@echo "	SEQ		${SEQ}"
+	@echo "	NR_IDENTITY	${NR_IDENTITY}"
+	time cd-hit -i ${SEQ} \
+		-d 0 -c ${NR_IDENTITY} -n 5 -G 1 -g 1 -b 20 -l 10 -s 0.0 -aL 0.0 -aS 0.0 -M ${NR_MEM} -T ${NR_THREADS} \
+		-o ${NR_SEQ}
+	@echo "	NR_SEQ		${NR_SEQ}"
+	@echo "		before filtering	${SEQ_NB}"
+	@echo "		after filtering		${NR_SEQ_NB}"
+
+nr_sars:
+	@${MAKE} nr TAXID=694009 TAXNAME=SARS
+
+# nr_sarbecovirus:
+# 	@${MAKE} nr TAXID=2509511 TAXNAME=sarbecovirus
+
+nr_betacoronaviridae:
+	@${MAKE} nr TAXID=694002 TAXNAME=betacoronavirus
+
+nr_coronaviridae:
+	@${MAKE} nr TAXID=11118 TAXNAME=coronaviridae
+
+#-c ${NR_IDENTITY} -n ${NR_WSIZE} -M ${NR_MEM} –d 1 -T ${NR_THREADS} \
 
 ################################################################
 ## Multiple alignment of spike protein sequences
 #DATA_DIR=analyses/spike_protein/data_spike-proteins/
 DATA_DIR=data/spike_proteins
-SPIKE_PREFIX=${TAXNAME}_${TAXID}_spike_prot
-SPIKE_SEQ=${DATA_DIR}/${SPIKE_PREFIX}
 #MUSCLE_DIR=analyses/spike_protein/muscle_alignments/
+MUSCLE_IN=${SPIKE_SEQ}
 MUSCLE_DIR=results/spike_protein/muscle_alignments/
 MUSCLE_PREFIX=${MUSCLE_DIR}/${SPIKE_PREFIX}_aligned_muscle
 MUSCLE_FORMAT=msf
 MUSCLE_LOG=${MUSCLE_PREFIX}_${MUSCLE_FORMAT}_log.txt
-MUSCLE_OPT=-quiet
+MUSCLE_MAXHOURS=1
+MUSCLE_OPT=-quiet -maxhours ${MUSCLE_MAXHOURS}
 align_muscle:
 	@echo "Spike proteins: multiple alignemnt with MUSCLE"
 	@echo "	Result directory"
 	@echo "	DATA_DIR		${DATA_DIR}"
-	@echo "	SPIKE_PREFIX		${SPIKE_PREFIX}"
 	@echo "	MUSCLE_DIR		${MUSCLE_DIR}"
 	@echo "	MUSCLE_PREFIX		${MUSCLE_PREFIX}"
 	@echo "	MUSCLE_LOG		${MUSCLE_LOG}"
 	@mkdir -p ${MUSCLE_DIR}
-	@${MAKE} _align_muscle_one_format MUSCLE_FORMAT=msf
-	@${MAKE} _align_muscle_one_format MUSCLE_FORMAT=html
+#	@${MAKE} _align_muscle_one_format MUSCLE_FORMAT=msf
 	@${MAKE} _align_muscle_one_format MUSCLE_FORMAT=clw
+	@${MAKE} _align_muscle_one_format MUSCLE_FORMAT=html
 
 _align_muscle_one_format:
-	time muscle -in ${SPIKE_SEQ}.fasta -${MUSCLE_FORMAT} ${MUSCLE_OPT} -log ${MUSCLE_LOG} -out ${MUSCLE_PREFIX}.${MUSCLE_FORMAT}
+	time muscle -in ${MUSCLE_IN}.fasta -${MUSCLE_FORMAT} ${MUSCLE_OPT} -log ${MUSCLE_LOG} -out ${MUSCLE_PREFIX}.${MUSCLE_FORMAT}
 	@echo "	${MUSCLE_PREFIX}.${MUSCLE_FORMAT}"
 
-align_uniprot_seq:
-	@echo "Aligning spike sequences from Uniprot"
-	@${MAKE} align_muscle SPIKE_PREFIX=uniprot_SARS_spike_taxid-694009_complete-seq_174_proteins
+align_uniprot_coronaviridae:
+	@echo "Aligning all Uniprot coronaviridae sequences"
+	@${MAKE} align_muscle TAXID=11118 TAXNAME=coronaviridae
 
-align_selected_seq:
+align_uniprot_betacoronaviridae:
+	@echo "Aligning all Uniprot betacoronaviridae sequences"
+	@${MAKE} align_muscle TAXID=694002 TAXNAME=betacoronavirus
+
+align_uniprot_sars:
+	@echo "Aligning all Uniprot SARS sequences"
+	@${MAKE} align_muscle TAXID=694009 TAXNAME=SARS
+
+# align_uniprot:
+# 	@echo "Aligning spike sequences from Uniprot"
+# 	@${MAKE} align_muscle SPIKE_PREFIX=uniprot_SARS_spike_taxid-694009_complete-seq_174_proteins
+
+align_selected:
 	@echo "Aligning selected sequences"
 	@${MAKE} align_muscle SPIKE_PREFIX=selected_coronavirus_spike_proteins
 
-align_genbank_seq:
+align_genbank:
 	@echo "Aligning selected sequences from Genbank"
 	@${MAKE} align_muscle SPIKE_PREFIX=genbank_CoV_spike_sequences
 
