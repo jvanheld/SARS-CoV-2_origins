2ab670de78b3192b2f54b0f6f025d6ae28f59709
esallard <erw.sallard@gmail.com>
2020-05-08 21:37:11 +0200

    correction of the ACE2 analysis program

--- scripts/makefiles/05_ACE2_analysis.mk
+++ scripts/makefiles/05_ACE2_analysis.mk
@@ -11,40 +11,29 @@ targets:
 
 ## Multiple alignment of the selected ACE2 protein sequences
 MUSCLE_IN = data/ACE2/ACE2.fa
-MUSCLE_MAXHOURS=1
-MUSCLE_OPT=-quiet -maxhours ${MUSCLE_MAXHOURS}
-MUSCLE_DIR = results/ACE2_protein/
+MUSCLE_DIR = results/ACE2_protein
 MUSCLE_PREFIX=${MUSCLE_DIR}/ACE2_aligned_muscle
-MUSCLE_LOG=${MUSCLE_PREFIX}_${MUSCLE_FORMAT}_log.txt
 
 align_muscle_fasta:
 	@echo "ACE2 proteins: multiple alignemnt with MUSCLE"
 	@echo "	Result directory"
 	@echo "	MUSCLE_DIR		${MUSCLE_DIR}"
 	@echo "	MUSCLE_PREFIX		${MUSCLE_PREFIX}"
-	@echo "	MUSCLE_LOG		${MUSCLE_LOG}"
 	@mkdir -p ${MUSCLE_DIR}
-	@${MAKE} _muscle_alignment MUSCLE_FORMAT= MUSCLE_EXT=fa
-
-_muscle_alignment:
-	time muscle -in ${MUSCLE_IN} ${MUSCLE_FORMAT} ${MUSCLE_OPT} \
-		-seqtype protein \
-		-log ${MUSCLE_LOG} \
-		-out ${MUSCLE_PREFIX}.${MUSCLE_EXT}
-	@echo "	${MUSCLE_PREFIX}.${MUSCLE_EXT}"
+	@ muscle -in ${MUSCLE_IN} -out ${MUSCLE_PREFIX}.fa
 
 ## Construction of the ACE2 phylogenetic tree from the multiple alignment with phyML
 
 # to be completed
 
 ## identification of the similarity of each ACE2 protein with the human ACE2 on residues critical for SARS-CoV-2 spike binding
-MULTIPLE_ALIGNMENT=${MUSCLE_PREFIX}.${MUSCLE_EXT}
+MULTIPLE_ALIGNMENT=${MUSCLE_PREFIX}.fa
 OUTPUT=${MUSCLE_DIR}/ACE2_comparison_with_${REFERENCE}.csv
 
 _compare_ACE2:
 	@echo "comparison of the important residues in "
 	@echo "${MULTIPLE_ALIGNMENT}"
-	@echo " with the sequence #
+	@echo " with the sequence "
 	@echo "${REFERENCE}"
 	@echo ". Output file: "
 	@echo "${OUTPUT}"
