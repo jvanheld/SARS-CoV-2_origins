43b861658e7d542e1f82c894df00867013bb40da
esallard <erw.sallard@gmail.com>
2020-05-09 16:18:10 +0200

    ACE2 script completed and results

--- scripts/makefiles/05_ACE2_analysis.mk
+++ scripts/makefiles/05_ACE2_analysis.mk
@@ -6,7 +6,8 @@ MAKE=make -f ${MAKEFILE}
 
 targets:
 	@echo "Targets:"
-	@echo "align_muscle_fasta:		align ACE2 protein sequences with muscle"
+	@echo "align_muscle_fasta_phylip:	align ACE2 protein sequences with muscle. Outputs given in fasta and phylip formats"
+	@echo "phyml_ACE2:			constructs a tree from the muscle alignment"
 	@echo "compare_ACE2_with_human:		calculates the number of differences of each aligned ACE2 protein with human ACE2 on the residues important for SARS-CoV-2 binding"
 
 ## Multiple alignment of the selected ACE2 protein sequences
@@ -14,17 +15,19 @@ MUSCLE_IN = data/ACE2/ACE2.fa
 MUSCLE_DIR = results/ACE2_protein
 MUSCLE_PREFIX=${MUSCLE_DIR}/ACE2_aligned_muscle
 
-align_muscle_fasta:
+align_muscle_fasta_phylip:
 	@echo "ACE2 proteins: multiple alignemnt with MUSCLE"
 	@echo "	Result directory"
 	@echo "	MUSCLE_DIR		${MUSCLE_DIR}"
 	@echo "	MUSCLE_PREFIX		${MUSCLE_PREFIX}"
 	@mkdir -p ${MUSCLE_DIR}
-	@ muscle -in ${MUSCLE_IN} -out ${MUSCLE_PREFIX}.fa
+	@ muscle -in ${MUSCLE_IN} -fastaout ${MUSCLE_PREFIX}.fa -phyiout ${MUSCLE_PREFIX}.phy
 
 ## Construction of the ACE2 phylogenetic tree from the multiple alignment with phyML
+NUMBER_BOOTSTRAP=50
 
-# to be completed
+phyml_ACE2:
+	@ phyml -i ${MUSCLE_PREFIX}.phy -d aa -b ${NUMBER_BOOTSTRAP}
 
 ## identification of the similarity of each ACE2 protein with the human ACE2 on residues critical for SARS-CoV-2 spike binding
 MULTIPLE_ALIGNMENT=${MUSCLE_PREFIX}.fa
